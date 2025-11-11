82e2ba3a8f548380d1ff3410ae5afaaf8f39d71f
LorenaVennemann <jil.vennemann@gmail.com>
2025-11-11 11:52:46 +0100

    Deploy React build to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: false,
+    emptyOutDir: true
   }
 })
